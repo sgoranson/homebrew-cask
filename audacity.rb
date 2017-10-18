e3137585710dd22f896d35a7a0287f7951e5752c
Steve Goranson <stephen.goranson@gmail.com>
2017-10-18 12:19:09 -0400

    added audacity brew

---
+++ audacity.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'audacity' do
+  version '2.1.3'
+  sha256 '68e82a944a9aa29068e2a2faa4cbd85f909d48f3916e6a57983d14f605d88b5d'
+
+  url "https://www.fosshub.com/Audacity.html/audacity-macos-#{version}.dmg"
+  name 'Audacity'
+  homepage 'https://www.fosshub.com/'
+  license :gpl
+
+  app 'Audacity/Audacity.app'
+end
